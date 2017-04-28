1f30fc0a66194be323867c6c6573252fb1d57c9b
lightsing <light.tsing@gmail.com>
2017-04-28 23:51:54 +0800

    Add config

---
+++ config_example.py
@@ -0,0 +1,10 @@
+import logging
+
+DEBUG = True
+HOST = None
+PORT = None
+DATABASE = 'database.db'
+DATABASE_INIT = 'database.sql'
+LOG_FILE = ''
+LOG_LEVEL = logging.DEBUG
+LOG_FORMAT = '%(asctime)s %(levelname)s:%(message)s'
